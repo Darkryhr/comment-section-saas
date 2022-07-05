f4a61be535e187537e875129c1aee0ead09cce2c
Gabriel Lellouche <gabyk800@gmail.com>
2022-07-05 19:02:30 +0300

    added chakra + theme + custom document

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Button, Heading } from '@chakra-ui/react';
+
+import { useAuth } from '@lib/auth';
 
 const Home = () => {
-  return <div>Home</div>;
+  const auth = useAuth();
+  return (
+    <div>
+      <Heading>Comment Section</Heading>
+      <div>
+        {auth?.user ? (
+          <Button onClick={e => auth.signout()}>Sign Out</Button>
+        ) : (
+          <Button onClick={e => auth.signinWithGithub()}>Sign In</Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
