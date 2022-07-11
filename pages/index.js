edf608b74d4dd3efdb8373b06e9ef01d7d8dde50
Gabriel Lellouche <gabyk800@gmail.com>
2022-07-11 18:21:56 +0300

    dashboard button

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,21 @@ const Home = () => {
       <Heading>Comment Section</Heading>
       <div>
         {auth?.user ? (
-          <Button mt={4} size='sm' onClick={e => auth.signout()}>
-            Sign Out
+          <Button
+            as='a'
+            href='/sites'
+            backgroundColor='gray.900'
+            color='white'
+            fontWeight='medium'
+            mt={4}
+            maxW='200px'
+            _hover={{ bg: 'gray.700' }}
+            _active={{
+              bg: 'gray.800',
+              transform: 'scale(0.95)',
+            }}
+          >
+            View Dashboard
           </Button>
         ) : (
           <Button mt={4} size='sm' onClick={e => auth.signinWithGithub()}>
